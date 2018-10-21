16df32eee680450a842335e4b2c6d2ac1eb76a7a
李敏 <13693576511@163.com>
2018-10-22 07:10:50 +0800

    初始化项目

---
+++ hsitory/1.index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+// render的含义就是把一个React元素渲染到Dom容器内部
+//jsx javascript+html(xml的一种)的混合写法
+ReactDom.render(
+    <h1>hello</h1>,
+    document.querySelector('#root')
+);
+// react元素是通过JS对象来描述DOM结构的一种数据结构
+// {
+//     tagName:'h1',
+//     attr:null,
+//     chaildren:['hello']
+// }
+// React.render(
+//     React.createElement('h1',null,['helol'])
+// )
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
+// 声明了一个React元素
+let ele=<h1>hello</h1>
+console.log(ele);
+// 1、标签类型 属性对象 子元素
+// 1.此处写的属性名都要转成驼峰命名法
+// 2.有些属性是JS关键字，要换种写法class--className  for-htmlFor
+let _ele2=React.createElement('div',{className:"red"},['hello']);
+console.log(_ele2);
+// 最终的React元素是这样的
+let eleObj={
+    type:'div',props:{
+        className:'red',
+        children:['hello']
+    }
+};
+function render(){
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+}
+// jsx语法
+let ele2=<div><span>hello11</span></div>
+ReactDom.render(_ele2,document.querySelector('#root'));
