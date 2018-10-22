cc77f41f2d2b97ddfadaf0c60b3ec362d39479c0
李敏 <13693576511@163.com>
2018-10-22 20:59:55 +0800

    init

--- src/index.js
+++ src/index.js
@@ -6,18 +6,28 @@ console.log(ele);
 // 1、标签类型 属性对象 子元素
 // 1.此处写的属性名都要转成驼峰命名法
 // 2.有些属性是JS关键字，要换种写法class--className  for-htmlFor
-let _ele2=React.createElement('div',{className:"red"},['hello']);
+let _ele2=React.createElement('div',{id:1,className:"red"},['hello']);
 console.log(_ele2);
 // 最终的React元素是这样的
 let eleObj={
     type:'div',props:{
         className:'red',
+        id:1,
         children:['hello']
     }
 };
-function render(){
-
+function render(eleObj,container){
+  // let {type,props}=eleObj;
+  // let ele=document.createElement(type);
+  // for(let attr in props){
+  //     if(attr=='children'){
+  //
+  //     }else{
+  //         ele.setAttribute(attr,props[attr])
+  //     }
+  //     ele.setAttribute(attr,props[attr])
+  // }
 }
 // jsx语法
 let ele2=<div><span>hello11</span></div>
-ReactDom.render(_ele2,document.querySelector('#root'));
+render(eleObj,document.querySelector('#root'));
